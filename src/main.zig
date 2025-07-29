67dee5884b68ca4c1064aae8f465a781d7df8dea
D4-CTM <josuedelcid325@gmail.com>
2025-07-29 17:56:41 -0600

    Initial project state

---
+++ src/main.zig
@@ -0,0 +1,9 @@
+//! This is a simple implementation of an AST (abstract syntax tree) in ziglang
+//! for parsing numeric operations and storing variable.
+const std = @import("std");
+
+const print = std.debug.print;
+
+pub fn main() void {
+    print("Hola, {s}!\n", .{ "mundo" });
+}
---
+++ src/test.zig
@@ -0,0 +1,8 @@
+const std = @import("std");
+
+const expect = std.testing.expect;
+const expectEqual = std.testing.expectEqual;
+
+test "Test sample" {
+    try expect(true);
+}
